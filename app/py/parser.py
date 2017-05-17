96f569bf43d6d97b76b2e2de0beed4ca9c870970
eveafeline <eveafeline@gmail.com>
2017-05-16 21:35:50 -0700

    modified csv parser to python 3

--- app/py/parser.py
+++ app/py/parser.py
@@ -12,23 +12,23 @@ def isfloat(value):
         return False
 
 def readCsvFile(fileName):
-    csvFilePointer = open(fileName, 'rb')
-    dataStream = csv.reader(csvFilePointer)
-    headers = dataStream.next()
-    datas = []
-    for row in dataStream:
-        data = {}
-        for h, v in zip(headers, row):
-            if isfloat(v):
-                v = float(v)
-            data[h] = v
-        datas.append(data)
+    with open(fileName) as csvfile:
+        datas = []
+        readCSV = csv.reader(csvfile, delimiter=',')
+        header = []
+        for idx, row in enumerate(readCSV):
+            data = {}
+            if idx == 0:
+                header = row
+            if idx > 0:
+                for i, val in enumerate(row):
+                    if isfloat(row[i]):
+                        row[i] = float(row[i])
+                    data[header[i]] = row[i]
+                datas.append(data)
     return datas
 
 def readJsonFile(fileName):
     with open(fileName) as data_file:
         datas = json.dumps(json.loads(data_file.read()))
     return datas
-
-storage = fileName
-print(storage)
